0fd911eaa0335ac5b91dde02b13a49bdad23e51d
Alexander Kuzmenko <alex@stablex.ru>
2014-09-15 23:24:13 +0400

    date-time parts calculations

--- DateTime.hx
+++ DateTime.hx
@@ -3,6 +3,10 @@ package ;
 
 
 abstract DateTime (Int) from Int to Int {
+    /** Amount of seconds in one minute */
+    static public inline var SECONDS_PER_MINUTE = 60;
+    /** Amount of seconds in one hour */
+    static public inline var SECONDS_PER_HOUR = 3600;
     /** Seconds per day */
     static public var SECONDS_PER_DAY = 86400;
     /** Amount of sconds in year */
@@ -26,6 +30,21 @@ abstract DateTime (Int) from Int to Int {
         2678400, //Oct
         2592000, //Nov
         2678400  //Dec
+    ];
+    /** amount of days in each month */
+    static private var dpm : Array<Int> = [
+        31, //Jan
+        28, //Feb
+        31, //Mar
+        30, //Apr
+        31, //May
+        30, //Jun
+        31, //Jul
+        31, //Aug
+        30, //Sep
+        31, //Oct
+        30, //Nov
+        31  //Dec
     ];
 
 
@@ -121,11 +140,72 @@ abstract DateTime (Int) from Int to Int {
 
 
     /**
+    * Get day number (1-31)
+    *
+    */
+    public inline function getDay () : Int {
+        var days : Int = Std.int( (this - yearStart()) / SECONDS_PER_DAY ) + 1;
+
+        var day : Int = 1;
+        for (m in 0...dpm.length) {
+
+            if (m == 1 && isLeapYear()) {
+                if (days - dpm[m] - 1 <= 0) {
+                    day = days;
+                    break;
+                }
+                days -= dpm[m] + 1;
+            } else {
+                if (days - dpm[m] <= 0) {
+                    day = days;
+                    break;
+                }
+                days -= dpm[m];
+            }
+        }
+
+        return day;
+    }//function getDay()
+
+
+    /**
+    * Get hour number (0-23)
+    *
+    */
+    public inline function getHour () : Int {
+        var days : Int = Std.int(this / SECONDS_PER_DAY);
+        return Std.int((this - days * SECONDS_PER_DAY) / SECONDS_PER_HOUR);
+    }//function getHour()
+
+
+    /**
+    * Get minumte number (0-59)
+    *
+    */
+    public inline function getMinute () : Int {
+        var hours : Int = Std.int(this / SECONDS_PER_HOUR);
+        return Std.int((this - hours * SECONDS_PER_HOUR) / SECONDS_PER_MINUTE);
+    }//function getMinute()
+
+
+    /**
+    * Get second number (0-59)
+    *
+    */
+    public inline function getSecond () : Int {
+        var minutes : Int = Std.int(this / SECONDS_PER_MINUTE);
+        return this - minutes * SECONDS_PER_MINUTE;
+    }//function getSecond()
+
+
+    /**
     * Convert to string representation in format YYYY-MM-DD HH:MM:SS
     *
     */
     // public inline function toString () : String {
 
     // }//function toString()
+
+
 
 }//abstract DateTime
--- test/Test.hx
+++ test/Test.hx
@@ -11,14 +11,17 @@ import haxe.unit.TestRunner;
 */
 class Test extends TestCase {
 
-    /** 2014-09-15 21:51:35 */
+    /** 2014-09-15 17:51:35 */
     static public inline var STAMP_01 = 1410803495;
     /** 1973-01-01 00:00:00 */
     static public inline var STAMP_02 = 94694400;
+    /** 2014-08-31 23:59:59 */
+    static public inline var STAMP_03 = 1409529599;
     /** 2012-02-29 00:00:00 */
-    static public inline var STAMP_03 = 1330473600;
+    static public inline var STAMP_04 = 1330473600;
     /** 1972-02-29 23:59:59 */
-    static public inline var STAMP_04 = 68255999;
+    static public inline var STAMP_05 = 68255999;
+
 
 
     /**
@@ -26,6 +29,9 @@ class Test extends TestCase {
     *
     */
     static public function main () : Void {
+        // Sys.command('php', ['-r', 'date_default_timezone_set("UTC");echo date("Y-m-d H:i:s", 1410803495);']);return;
+        // Sys.command('php', ['-r', 'date_default_timezone_set("UTC");echo strtotime("2014-08-31 23:59:59");']);return;
+
         var runner = new TestRunner();
         runner.add(new Test());
         runner.run();
@@ -38,7 +44,7 @@ class Test extends TestCase {
     */
     public function testYear () : Void {
 
-        // 2014-09-15 21:51:35
+        // 2014-09-15 17:51:35
         var dt = new DateTime(STAMP_01);
         assertEquals(44, dt.getUnixYear());
         assertEquals(2014, dt.getYear());
@@ -52,15 +58,22 @@ class Test extends TestCase {
         assertFalse(dt.isLeapYear());
         assertEquals(STAMP_02, dt.yearStart());
 
-        // 2012-02-29 00:00:00
+        // 2014-08-31 23:59:59
         var dt = new DateTime(STAMP_03);
+        assertEquals(44, dt.getUnixYear());
+        assertEquals(2014, dt.getYear());
+        assertFalse(dt.isLeapYear());
+        assertEquals(1388534400, dt.yearStart());
+
+        // 2012-02-29 00:00:00
+        var dt = new DateTime(STAMP_04);
         assertEquals(42, dt.getUnixYear());
         assertEquals(2012, dt.getYear());
         assertTrue(dt.isLeapYear());
         assertEquals(1325376000, dt.yearStart());
 
         // 1972-02-29 23:59:59
-        var dt = new DateTime(STAMP_04);
+        var dt = new DateTime(STAMP_05);
         assertEquals(2, dt.getUnixYear());
         assertEquals(1972, dt.getYear());
         assertTrue(dt.isLeapYear());
@@ -74,7 +87,7 @@ class Test extends TestCase {
     *
     */
     public function testMonth () : Void {
-        // 2014-09-15 21:51:35
+        // 2014-09-15 17:51:35
         var dt = new DateTime(STAMP_01);
         assertEquals(9, dt.getMonth());
 
@@ -82,15 +95,127 @@ class Test extends TestCase {
         var dt = new DateTime(STAMP_02);
         assertEquals(1, dt.getMonth());
 
-        // 2012-02-29 00:00:00
+        // 2014-08-31 23:59:59
         var dt = new DateTime(STAMP_03);
+        assertEquals(8, dt.getMonth());
+
+        // 2012-02-29 00:00:00
+        var dt = new DateTime(STAMP_04);
         assertEquals(2, dt.getMonth());
 
         // 1972-02-29 23:59:59
-        var dt = new DateTime(STAMP_04);
+        var dt = new DateTime(STAMP_05);
         assertEquals(2, dt.getMonth());
 
     }//function testMonth()
 
+
+    /**
+    * Test day-related methods
+    *
+    */
+    public function testDay () : Void {
+        // 2014-09-15 17:51:35
+        var dt = new DateTime(STAMP_01);
+        assertEquals(15, dt.getDay());
+
+        // 1973-01-01 00:00:00
+        var dt = new DateTime(STAMP_02);
+        assertEquals(1, dt.getDay());
+
+        // 2014-08-31 23:59:59
+        var dt = new DateTime(STAMP_03);
+        assertEquals(31, dt.getDay());
+
+        // 2012-02-29 00:00:00
+        var dt = new DateTime(STAMP_04);
+        assertEquals(29, dt.getDay());
+
+        // 1972-02-29 23:59:59
+        var dt = new DateTime(STAMP_05);
+        assertEquals(29, dt.getDay());
+
+    }//function testDay()
+
+
+    /**
+    * Test hour-related methods
+    *
+    */
+    public function testHour () : Void {
+        // 2014-09-15 17:51:35
+        var dt = new DateTime(STAMP_01);
+        assertEquals(17, dt.getHour());
+
+        // 1973-01-01 00:00:00
+        var dt = new DateTime(STAMP_02);
+        assertEquals(0, dt.getHour());
+
+        // 2014-08-31 23:59:59
+        var dt = new DateTime(STAMP_03);
+        assertEquals(23, dt.getHour());
+
+        // 2012-02-29 00:00:00
+        var dt = new DateTime(STAMP_04);
+        assertEquals(0, dt.getHour());
+
+        // 1972-02-29 23:59:59
+        var dt = new DateTime(STAMP_05);
+        assertEquals(23, dt.getHour());
+    }//function testHour()
+
+
+    /**
+    * Test minute-related methods
+    *
+    */
+    public function testMinute () : Void {
+        // 2014-09-15 17:51:35
+        var dt = new DateTime(STAMP_01);
+        assertEquals(51, dt.getMinute());
+
+        // 1973-01-01 00:00:00
+        var dt = new DateTime(STAMP_02);
+        assertEquals(0, dt.getMinute());
+
+        // 2014-08-31 23:59:59
+        var dt = new DateTime(STAMP_03);
+        assertEquals(59, dt.getMinute());
+
+        // 2012-02-29 00:00:00
+        var dt = new DateTime(STAMP_04);
+        assertEquals(0, dt.getMinute());
+
+        // 1972-02-29 23:59:59
+        var dt = new DateTime(STAMP_05);
+        assertEquals(59, dt.getMinute());
+    }//function testMinute()
+
+
+    /**
+    * Test second-related methods
+    *
+    */
+    public function testSecond () : Void {
+        // 2014-09-15 17:51:35
+        var dt = new DateTime(STAMP_01);
+        assertEquals(35, dt.getSecond());
+
+        // 1973-01-01 00:00:00
+        var dt = new DateTime(STAMP_02);
+        assertEquals(0, dt.getSecond());
+
+        // 2014-08-31 23:59:59
+        var dt = new DateTime(STAMP_03);
+        assertEquals(59, dt.getSecond());
+
+        // 2012-02-29 00:00:00
+        var dt = new DateTime(STAMP_04);
+        assertEquals(0, dt.getSecond());
+
+        // 1972-02-29 23:59:59
+        var dt = new DateTime(STAMP_05);
+        assertEquals(59, dt.getSecond());
+    }//function testSecond()
 
 }//class Test
