6827148b204e02ed65fa706c25d1c29aa476bd4c
Alexander Kuzmenko <alex@stablex.ru>
2017-06-08 13:54:51 +0300

    dox script update

--- dox.sh
+++ dox.sh
@@ -1,4 +1,4 @@
 #!/bin/dash
 
 haxe -xml build/doc.xml -cp src -cp test -main Test
-haxelib run dox -i build -o build/dox -ex '/cores\|cores\|TZAbr\|data\|Decoder\|IntervalUtils\|MonthUtils\|SnapUtils\|MacroUtils\|neDetect\|zoneUtils\|TimeUtils\|TimeUtils/i'
+haxelib run dox -i build -o build/dox -ex "/cores|cores|TZAbr|data|Decoder|IntervalUtils|MonthUtils|SnapUtils|MacroUtils|neDetect|zoneUtils|TimeUtils|TimeUtils/i"
